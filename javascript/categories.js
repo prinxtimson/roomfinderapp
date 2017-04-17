9c7defae60dd36db720a5871b708c1436ab3fe2b
Timson Babajide <timson.babajide@gmail.com>
2017-04-17 03:02:57 +0100

    little adjusment in the DB part

--- javascript/categories.js
+++ javascript/categories.js
@@ -2,27 +2,28 @@
 
 var roomData = {
 	meetings: {
-		desc: "A room for holding meetings";
-		capacity: 10;
-		bookedRooms: 0;
+		desc: "A room for holding meetings",
+		capacity: 10,
+		bookedRooms: 0
 	},
 	games: {
-		desc: "Room with a 42 inch HD TV for gaming";
-		capacity: 8;
-		bookedRooms: 7;
+		desc: "Room with a 42 inch HD TV for gaming",
+		capacity: 8,
+		bookedRooms: 7
 	}, 
 	quiet_time: {
-		desc: "A room for deep thoughts";
-		capacity: 4;
-		bookedRooms: 1;
+		desc: "A room for deep thoughts",
+		capacity: 4,
+		bookedRooms: 1,
+	},
 	learning: {
-		desc: "For serious minded people who want to learn";
-		capacity: 3;
-		bookedRooms: 3;
+		desc: "For serious minded people who want to learn",
+		capacity: 3,
+		bookedRooms: 3,
 	},
 	working: {
-		desc: "For productive people who have a lot of deliverables to deliver";
-		capacity: 20;
-		bookedRooms: 18;
+		desc: "For productive people who have a lot of deliverables to deliver",
+		capacity: 20,
+		bookedRooms: 18,
 	}
 }
